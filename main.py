da98116cbaf591ae6d4a8b5c6bf7d057d21df833
sharanmanne <manne.sharan01@gmail.com>
2021-08-12 12:49:01 +0530

    Updated Repository

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 """
 This folder is main folder in Repository
 """
+def is_prime(number: int):
+    """
+    In This function we check the number is prime or not
+    returns:
+        True if prime False otherwise
+    """
 
 if "__init__"== "__main__":
     pass
